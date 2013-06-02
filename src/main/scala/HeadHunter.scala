ead6a571a8a88be6633651add3b56f8a98079ec9
Joe Barnes <barnesjd@gmail.com>
2013-06-02 13:02:00 -0500

    Added a 'HeadHunter' that doesn't do Options but returns the item

--- src/main/scala/HeadHunter.scala
+++ src/main/scala/HeadHunter.scala
@@ -1,12 +1,33 @@
 import shapeless._
   
-object header extends (Seq ~> Option) {
+object headerOption extends (Seq ~> Option) {
   def apply[T](s : Seq[T]) = s.headOption
 }
 
-object HeadHunter {
+object HeadHunterOption {
   def get[A](a:Seq[A]):Option[A] = 
     a.headOption
+    
+  def get[InT <: Product, OutT <: Product, InL <: HList, OutL <: HList](t : InT)
+    (implicit hl : HListerAux[InT, InL], 
+     mapper : MapperAux[headerOption.type, InL, OutL],
+     tupler : TuplerAux[OutL, OutT]) = {
+    val h = hl(t)
+    val o = h map headerOption
+    val r = o.tupled  
+    r
+  }
+}
+
+import TypeOperators._
+
+object header extends (Seq ~> Id) {
+  def apply[T](s : Seq[T]) = s.head
+}
+
+object HeadHunter {
+  def get[A](a:Seq[A]):A = 
+    a.head
     
   def get[InT <: Product, OutT <: Product, InL <: HList, OutL <: HList](t : InT)
     (implicit hl : HListerAux[InT, InL], 
--- src/test/scala/HeadHunterSuite.scala
+++ src/test/scala/HeadHunterSuite.scala
@@ -3,9 +3,9 @@ import org.scalatest.matchers.ShouldMatchers
 import shapeless._
 
 class HeadHunterSuite extends WordSpec with ShouldMatchers {
-  "HeadHunter" should {
+  "HeadHunterOption" should {
     "be type safe and therefore awesome" in {
-      import HeadHunter._
+      import HeadHunterOption._
       val t1:Option[Int] = get(Seq(1,2,3))
       t1 should equal (Some(1))
       
@@ -14,6 +14,20 @@ class HeadHunterSuite extends WordSpec with ShouldMatchers {
       
       val t3:(Option[Boolean], Option[String], Option[Int]) = get(Seq(true,false,true), Seq("str1","str2"), Seq(3)) 
       t3 should equal ((Some(true), Some("str1"), Some(3)))
+    }
+  }
+
+  "HeadHunter" should {
+    "be type safe and also awesome" in {
+      import HeadHunter._
+      val t1:Int = get(Seq(1,2,3))
+      t1 should equal (1)
+      
+      val t2:(String, Int) = get((Seq("strs","str2"), Seq(4,5,6)))
+      t2 should equal (("strs",4))
+      
+      val t3:(Boolean, String, Int) = get(Seq(true,false,true), Seq("str1","str2"), Seq(3)) 
+      t3 should equal ((true, "str1", 3))
     }
   }
 }
